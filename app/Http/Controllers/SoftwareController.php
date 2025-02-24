b9ecd85188b0dde85d37b6cf01de911a9f5f96cc
Abdelilah <abdouajjouqa@gmail.com>
2025-02-24 15:26:56 +0000

    add software's model

---
+++ app/Http/Controllers/SoftwareController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\software;
+use App\Http\Requests\StoresoftwareRequest;
+use App\Http\Requests\UpdatesoftwareRequest;
+
+class SoftwareController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoresoftwareRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(software $software)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(software $software)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdatesoftwareRequest $request, software $software)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(software $software)
+    {
+        //
+    }
+}
