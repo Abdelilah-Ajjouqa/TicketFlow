2ee78c00af9076c2d7dbf1f12c87a0232d8a7d04
Abdelilah <abdouajjouqa@gmail.com>
2025-02-24 14:57:38 +0000

    add developer's model

---
+++ app/Http/Controllers/DevelopersController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\developers;
+use App\Http\Requests\StoredevelopersRequest;
+use App\Http\Requests\UpdatedevelopersRequest;
+
+class DevelopersController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoredevelopersRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(developers $developers)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(developers $developers)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdatedevelopersRequest $request, developers $developers)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(developers $developers)
+    {
+        //
+    }
+}
