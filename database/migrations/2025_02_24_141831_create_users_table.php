3cdfa6a6d58348ddc9a36319dd9d811c36c7f1c2
Abdelilah <abdouajjouqa@gmail.com>
2025-02-25 11:11:13 +0000

    add the columns needed for users table, and add the foreign keys to others tables

--- database/migrations/2025_02_24_141831_create_users_table.php
+++ database/migrations/2025_02_24_141831_create_users_table.php
@@ -13,6 +13,11 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->string('email')->unique();
+            $table->string('password');
+            $table->enum('role', ['client', 'developer', 'admin']);
+            $table->rememberToken();
             $table->timestamps();
         });
     }
