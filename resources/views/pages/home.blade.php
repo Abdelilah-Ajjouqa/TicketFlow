948e50ff6ed58ed17900f7e8476da907084f0275
Abdelilah <abdouajjouqa@gmail.com>
2025-02-25 09:20:46 +0000

    fix the conflie

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -1,18 +1,3 @@
-<<<<<<< HEAD
 <x-header>
     <h1>Here you are</h1>
 </x-header>
-=======
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>TicketFlow | Home</title>
-</head>
-<body>
-    <h1>you do it</h1>
-</body>
-</html>
->>>>>>> 749691133704ec6ad9e7744d8d086f583c852dd5
