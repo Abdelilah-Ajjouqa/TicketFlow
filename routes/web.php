3f5bdec4cd7628c35e81c7c32c663c85a17bbf36
Abdelilah <abdouajjouqa@gmail.com>
2025-02-28 11:51:25 +0000

    add logout route and implement client role middleware for authentication

--- routes/web.php
+++ routes/web.php
@@ -8,8 +8,10 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', [UsersController::class, 'index'])->name('home');
 
 // auth Routes
+
 Route::get('/register', [AuthController::class, 'showRegister'])->name('show.register');
 Route::get('/login', [AuthController::class, 'showLogin'])->name('show.login');
 
 Route::post('/register', [AuthController::class, 'register'])->name('register');
 Route::post('/login', [AuthController::class, 'login'])->name('login');
+Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
