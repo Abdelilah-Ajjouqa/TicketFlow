3eb0cf6a3442dac9d4f6281fadbe58559dab4a4f
Abdelilah <abdouajjouqa@gmail.com>
2025-02-28 17:29:13 +0000

    add ticket routes and create ticket view templates

--- routes/web.php
+++ routes/web.php
@@ -15,3 +15,16 @@ Route::get('/login', [AuthController::class, 'showLogin'])->name('show.login');
 Route::post('/register', [AuthController::class, 'register'])->name('register');
 Route::post('/login', [AuthController::class, 'login'])->name('login');
 Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
+
+// tickets Routes
+Route::get('/tickets', function () {
+    return view('tickets.index');
+})->name('tickets.index');
+
+Route::get('/tickets/create', function () {
+    return view('tickets.create');
+})->name('tickets.create');
+
+Route::get('/tickets/{ticket}', function () {
+    return view('tickets.show');
+})->name('tickets.show');
